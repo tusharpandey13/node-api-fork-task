12af5c9ec34130658a6d81e3be839141acb36b54
tusharpandey13 <tusharpandey13@gmail.com>
2020-06-07 23:22:10 +0530

    23:22:10

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 var routes = {
-	v1: { genericRoute: require('./routes/genericRoute.js') },
+	v1: { genericRoute: require('./v1/routes/genericRoute.js') },
 };
 
 // guaranteed to get dependencies
 module.exports = () => {
 	const app = require('express').Router();
 
-	// genericRoute(app);
 	// WARNING: DO NOT TOUCH
+	// uses the format ip:port/api/v{n}/{route}/{method}
+
 	for (const [k0, v0] of Object.entries(routes)) {
 		let tmp_app = require('express').Router();
 		app.use(`/${k0}`, tmp_app);
