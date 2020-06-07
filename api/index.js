7a800cb247bf66e6f52ae7f9f48d6fb505b056ea
tusharpandey13 <tusharpandey13@gmail.com>
2020-06-07 23:54:00 +0530

    23:54:00

--- api/index.js
+++ api/index.js
@@ -2,13 +2,13 @@ var routes = {
 	v1: { genericRoute: require('./v1/routes/genericRoute.js') },
 };
 
-// WARNING: DO NOT TOUCH
 // guaranteed to get dependencies
 module.exports = () => {
 	const app = require('express').Router();
 
 	// uses the format ip:port/api/v{n}/{route}/{method}
 
+	// WARNING: DO NOT TOUCH
 	for (const [k0, v0] of Object.entries(routes)) {
 		let tmp_app = require('express').Router();
 		app.use(`/${k0}`, tmp_app);
